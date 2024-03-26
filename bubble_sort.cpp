8c791c2ec7000635d46071c696c4ba1359d6768b
Tanvir <tanvir.roni1997@gmail.com>
2024-03-27 02:17:43 +0600

    Sorted an array using bubble sort algorithm

---
+++ bubble_sort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int len);
+void swap(int arr[], int a, int b);
+
+int main() {
+    int arr[5] = {5, 4, 3, 2, 1};
+    int len = sizeof(arr)/sizeof(arr[0]);
+    
+    bubbleSort(arr, len);
+    
+    for(int i=0; i<len; i++) {
+      cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void bubbleSort(int arr[], int len) {
+    for(int i=0; i<len-1; i++) {
+        for(int j=0; j<len-i-1; j++) {
+            if(arr[j+1] < arr[j]) {
+                swap(arr, j, j+1);
+            }
+        }
+    }
+}
+
+void swap(int arr[], int a, int b) {
+    int temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+}
